bd032b29189d5068b518aae03f78fe490b57d04d
Alex Barrios <ambarrio@andrew.cmu.edu>
2023-09-12 23:00:30 -0400

    Added more test cases for clear and dequeue to achieve 100% branch coverage

--- TypeScript/tests/queue.test.ts
+++ TypeScript/tests/queue.test.ts
@@ -8,48 +8,64 @@ let createQueue = newLinkedListIntQueue
 // TODOs:
 // write more test cases to test dequeue and clear functions.
 
-test("test isEmpty: newly created list should be empty", () => {
-    expect(createQueue().isEmpty()).toBeTruthy()
+test('test clear: remove all elements from queue', () => {
+  const queue = createQueue()
+  expect(queue.isEmpty()).toBeTruthy()
+  queue.enqueue(1)
+  expect(queue.isEmpty()).toBeFalsy()
+  queue.clear()
+  expect(queue.isEmpty()).toBeTruthy()
 })
 
-test("test isEmpty: list with 1 element is not empty", () => {
-    const queue = createQueue()
-    queue.enqueue(2)
-    expect(queue.isEmpty()).toBeFalsy()
+test('test dequeue: remove an element', () => {
+  const queue = createQueue()
+  expect(queue.dequeue()).toBeNull()
+  queue.enqueue(1)
+  expect(queue.dequeue()).toBe(1)
 })
 
-test("test peek: newly created list should peek null", () => {
-
-    expect(createQueue().peek()).toBeNull()
+test('test isEmpty: newly created list should be empty', () => {
+  expect(createQueue().isEmpty()).toBeTruthy()
 })
 
-test("test peek: queue with 2 element should peek the one that was most recently added", () => {
-    const queue = createQueue()
-    queue.enqueue(2)
-    queue.enqueue(3)
-    expect(queue.peek()).toEqual(3)
+test('test isEmpty: list with 1 element is not empty', () => {
+  const queue = createQueue()
+  queue.enqueue(2)
+  expect(queue.isEmpty()).toBeFalsy()
+})
+
+test('test peek: newly created list should peek null', () => {
+  expect(createQueue().peek()).toBeNull()
+})
+
+test('test peek: queue with 2 element should peek the one that was most recently added', () => {
+  const queue = createQueue()
+  queue.enqueue(2)
+  queue.enqueue(3)
+  expect(queue.peek()).toEqual(3)
 })
 
 let param = [5, 10, 1000000]
 // parameterized test, apply to each value of the parameter
-test.each(param)("test enqueue: enqueued number %d is correct", (nr) => {
-    const queue = createQueue()
-    queue.enqueue(nr)
-    expect(queue.peek()).toBe(nr)
+test.each(param)('test enqueue: enqueued number %d is correct', (nr) => {
+  const queue = createQueue()
+  queue.enqueue(nr)
+  expect(queue.peek()).toBe(nr)
 })
 
 // can nest tests with shared descriptions for better readability
-describe("test size: ", ()=> {
-    test("1 entry", ()=>{
-        const queue = createQueue()
-        queue.enqueue(5)
-        expect(queue.size()).toBe(1)
-    })
+describe('test size: ', ()=> {
+  test('1 entry', () => {
+    const queue = createQueue()
+    queue.enqueue(5)
+    expect(queue.size()).toBe(1)
+  })
 
-    test("11 entries", ()=>{
-        const queue = createQueue()
-        for (let i =0;i<11;i++)
-            queue.enqueue(i)
-        expect(queue.size()).toBe(11)
-    })
+  test('11 entries', () => {
+    const queue = createQueue()
+    for (let i = 0; i < 11; i++) {
+      queue.enqueue(i)
+    }
+    expect(queue.size()).toBe(11)
+  })
 })
